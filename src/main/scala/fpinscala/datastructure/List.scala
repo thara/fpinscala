9d901e74eb58106fa41c74ad6b0b793ca912cb8e
Tomochika Hara <tomochikahara@gmail.com>
2015-10-31 20:44:32 +0900

    Update List

--- src/main/scala/fpinscala/datastructure/List.scala
+++ src/main/scala/fpinscala/datastructure/List.scala
@@ -26,13 +26,14 @@ object List {
   def tail[A](l: List[A]): List[A] =
     l match {
       case Nil => Nil
-      case Cons(x, xs) => xs
+      case Cons(_, xs) => xs
     }
 
+  // EXERCISE 3.3
   def setHead[A](l: List[A], a: A): List[A] =
     l match {
       case Nil => Nil
-      case Cons(x, xs) => Cons(a, xs)
+      case Cons(_, xs) => Cons(a, xs)
     }
 
   // EXERCISE 3.4
@@ -40,13 +41,79 @@ object List {
     if (n == 0) l
     else l match {
       case Nil => Nil
-      case Cons(x, xs) => drop(xs, n-1)
+      case Cons(_, xs) => drop(xs, n-1)
     }
 
   // EXERCISE 3.5
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] =
     l match {
       case Nil => Nil
-      case Cons(x, xs) => if (f(x)) dropWhile(xs, f) else l
+      case Cons(x, xs) => if (f(x)) dropWhile(xs)(f) else l
     }
+
+  // EXERCISE 3.6
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(x, Nil) => Nil
+      case Cons(x, xs) => Cons(x, init(xs))
+    }
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]) = foldRight(ns, 0)((x, y)=> x + y)
+  def product2(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
+
+  // EXERCISE 3.7
+
+  // EXERCISE 3.9
+  def length[A](as: List[A]): Int = foldRight(as, 0)((x, n) => n + 1)
+
+  // EXERCISE 3.10
+  @annotation.tailrec
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+
+  // EXERCISE 3.11
+  def sum3(ns: List[Int]) = foldLeft(ns, 0)(_ + _)
+  def product3(ns: List[Double]) = foldLeft(ns, 1.0)(_ * _)
+  def length2[A](as: List[A]): Int = foldLeft(as, 0)((n, x) => n + 1)
+
+  // EXERCISE 3.12
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h, t) => Cons(h, append(t, a2))
+    }
+
+  def reverse[A](as: List[A]): List[A] =
+    as match {
+      case Nil => Nil
+      case Cons(x, Nil) => List(x)
+      case Cons(h, t) => append(reverse(t), List(h))
+    }
+
+  def reverse2[A](as: List[A]): List[A] =
+    foldRight(as, Nil:List[A])((x, l) => append(l, List(x)))
+
+  // EXERCISE 3.13
+  // def foldLeft2[A,B](as: List[A], z: B)(f: (B, A) => B): B =
+  //   foldRight(as, (y: B) => y)((f2, xs) => )
+    // as match {
+    //   case Nil => z
+    //   case Cons(x, xs) => foldRight(as, f(z, x))((acc, ys) => f(ys, acc))
+    // }
+
+  // EXERCISE 3.14
+  def append2[A](a1: List[A], a2: List[A]): List[A] =
+    foldRight(a1, a2)((x, as) => Cons(x, as))
+  // def append3[A](a1: List[A], a2: List[A]): List[A] =
+  //   foldLeft(a1, a2)((as, x) => Cons(x, as))
 }
